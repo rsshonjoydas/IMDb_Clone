c7d980949f30c8bd6afc9d31440c314dd4e50bf2
”rsshonjoydas” <rsshonjoydas@email.com>
2023-02-28 10:24:02 +0600

    ci: project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
+import Header from '@/components/Header';
+import { Children } from '@/types';
 import '../../styles/index.scss';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: Children) {
   return (
     <html lang='en'>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
