6eb08d3dd3b638c6da1d945283adbe6dcc6396c3
”rsshonjoydas” <rsshonjoydas@email.com>
2023-02-28 19:29:34 +0600

    ci: create the about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,36 @@
+const About = () => (
+  <div className='max-w-6xl p-4 mx-auto space-y-4'>
+    <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+    <p>
+      Welcome to our movie database website! As a team of dedicated movie enthusiasts, we have
+      pooled our expertise to bring you a comprehensive one-stop destination for all your
+      movie-related needs.
+    </p>
+
+    <p>
+      Our website boasts a vast and regularly updated database of movies from around the world,
+      complete with the latest news, reviews, and trailers. Our commitment to keeping our database
+      current ensures that you always have access to the most cutting-edge films and cinematic
+      content. With our advanced search functionality, you can easily search for movies by title,
+      director, actor, genre, or release date, making it a breeze to find the perfect movie for any
+      occasion.
+    </p>
+
+    <p>
+      In addition to our extensive database, we also offer a thriving community platform for movie
+      lovers to connect and engage with each other. Our community section features a forum where
+      like-minded individuals can discuss their favorite films, share their thoughts, and read
+      reviews and ratings from other users. Our movie news and trailers section keeps you up-to-date
+      with the latest developments in the world of cinema.
+    </p>
+
+    <p>
+      We thank you for choosing our website as your go-to source for all things movie-related.
+      Should you have any feedback or suggestions, please do not hesitate to contact us. We are
+      continually striving to improve the user experience on our website and look forward to
+      enhancing your browsing experience. Happy browsing!
+    </p>
+  </div>
+);
+
+export default About;
